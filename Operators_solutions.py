354067b7ab7fb7134c08225e5de7cd688192cfe1
Kurla Sreevalli <kurla.sreevalli@gmail.com>
2020-12-17 17:19:59 +0530

    Add files via upload

---
+++ Operators_solutions.py
@@ -0,0 +1,69 @@
+#1 1) What is the output of the following addition (+) operator
+#         a = [10, 20]
+# b = a
+# b += [30, 40]
+# print(a)
+# print(b)
+
+a = [10, 20]
+b = a
+b += [30, 40]
+print(a)
+print(b)
+
+#2 2) What is the output of print(2%6)
+# a)	  ValueError
+# b)	  0.33
+# c)	  2
+
+print(2%6)
+
+#3 3) What is the output of the following code:
+# print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+
+# a)	  True True False True
+# b)	  False True True True
+# c)	  True True False True
+# d)	  False True False True
+
+print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+
+#4 4) What is the output of the expression  print(-18 // 4) ?
+print(-18 // 4) 
+
+#5 5) What is the output of print(2 ** 3 ** 2) ?
+print(2 ** 3 ** 2) 
+
+#6 6) 4 is 100 in binary and 11 is 1011. What is the output of the following bitwise operators?
+a = 4
+b = 11
+print(a | b)
+print(a >> 2)
+print(bool(a>>2))
+
+#7 7) What is the value of the following Python Expression
+print(36 / 4)
+
+#8 8) What is the output of the following assignment operator
+y = 10
+x = y + 2
+print(x)
+
+#9 9) What is the output of print(2 * 3 ** 3 * 4)
+(2 * 3 ** 3 * 4)
+
+#10 10) Which of the following operators has the highest precedence?
+"""BODMAS
+1.	Parentheses
+2.	Exponentiation
+3.	Multiplication and both division operators have the same precedence, which is higher than addition and subtraction
+"""
+
+#11 11) What is the output of print(10 - 4 * 2) ?
+print(10 - 4 * 2) 
+
+#12 12) What is the output of the following code
+x = 6
+y = 2
+print(x ** y)
+print(x // y)
